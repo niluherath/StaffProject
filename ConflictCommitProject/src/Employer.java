d45f9e8fdc1eb6862884ca99e8f8df74d2494856
N <N@i7>
2016-07-29 09:58:56 +1200

    Created a class Employer. It has three variables. String name, String iD, String dob. It aslo has setters and getters for each variable.

---
+++ ConflictCommitProject/src/Employer.java
@@ -0,0 +1,27 @@
+
+public class Employer {
+	
+	private String name; 
+	private String iD;
+	private String dob;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getiD() {
+		return iD;
+	}
+	public void setiD(String iD) {
+		this.iD = iD;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+}
